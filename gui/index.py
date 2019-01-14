dbb2fde1f452b58525d09e21f11ed29c6d66c575
master <sonam@sonam.com>
2019-01-14 15:05:21 +0600

    edited readme

---
+++ gui/index.py
@@ -0,0 +1,6 @@
+import tkinter
+window = tkinter.Tk()
+window.title("CST Smart Sec")
+label = tkinter.Label(window, text="hello World").pack()
+#label for what is to be shown in the window
+window.mainloop()
