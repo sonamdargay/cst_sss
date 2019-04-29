0fdcb390257c86babbeb408fa10f985c37097fd7
suraj <suraj@sumukus.com>
2019-04-29 23:48:41 +0600

    update

--- gui/pyCode/main.py
+++ gui/pyCode/main.py
@@ -4,10 +4,7 @@
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
-# WARNING! All changes made in this file will be lost!
-import cv2
-import numpy as np
-import os
+# WARNING! All changes made in this file will be lost
 
 import sys
 import sqlite3
@@ -16,6 +13,7 @@ from ui_addNew import Ui_addNew
 from ui_update import Ui_update
 from ui_delete import Ui_remove
 from ui_view import Ui_view
+from recogniser import *
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
     def openAddNew(self):
@@ -58,7 +56,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.btn_play = QtWidgets.QPushButton(self.centralwidget)
         self.btn_play.setObjectName("btn_play")
         self.verticalLayout_2.addWidget(self.btn_play)
-        self.btn_play.clicked.connect(self.VideoPlayer)
+        self.btn_play.clicked.connect(VideoPlayer)
 
         self.btn_add = QtWidgets.QPushButton(self.centralwidget)
         self.btn_add.setObjectName("btn_add")
@@ -115,62 +113,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
             sys.exit()
         else:
             pass
-    def VideoPlayer(self):
-        recognizer = cv2.face.LBPHFaceRecognizer_create()
-        recognizer.read('../../trainer/trainer.yml')
-        cascadePath = "../../cascades/haarcascade_frontalface_default.xml"
-        faceCascade = cv2.CascadeClassifier(cascadePath);
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        #iniciate id counter
-        id = 0
-        # names related to ids: example ==> Marcelo: id=1,  etc
-        names = ['Taw', 'Sonam','Karma Samdrup','Paula', 'Ilza', 'Z', 'W','Suraj Mukhia','Rinchen','Ugyen'] 
-        # Initialize and start realtime video capture
-        cam = cv2.VideoCapture(0)
-        cam.set(3, 640) # set video widht
-        #cam.set(3, 820) #for rpi
-        cam.set(4, 480) # set video height
-        #cam.set(4, 740) #for rpi
-        # Define min window size to be recognized as a face
-        minW = 0.05*cam.get(3)
-        minH = 0.05*cam.get(4)
-        while True:
-            ret, img =cam.read()
-            #img = cv2.flip(img, -1) # Flip vertically
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            
-            faces = faceCascade.detectMultiScale( 
-                gray,
-                scaleFactor = 1.2,
-                minNeighbors = 5,
-                minSize = (int(minW), int(minH)),
-               )
-            for(x,y,w,h) in faces:
-                cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-                id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-                # Check if confidence is less them 100 ==> "0" is perfect match for 'confidence = "  {0}%".format(round(100 - confidence))'
-                # Confidence 
-                if (confidence < 100):
-                    id = names[id]
-                    #confidence = "  {0}%".format(round(100 - confidence))
-                    confidence = "  {0}%".format(round(confidence))
-                else:
-                    id = "unknown"
-                    #confidence = "  {0}%".format(round(100 - confidence))
-                    confidence = "  {0}%".format(round(100 - confidence))
-                
-                cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-                cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-
-            cv2.imshow('SSS-CST',img) 
-
-            k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-            if k == 27:
-                break
-        # Do a bit of cleanup
-        cam.release()
-        cv2.destroyAllWindows()
-
+    
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
--- mail.py
+++ mail.py
@@ -4,6 +4,6 @@ server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login("cst.sss101@gmail.com", "Raspberrypi")
 server.sendmail(
   "cst.sss101@gmail.com", 
-  "sonamdargay167@gmail.com", 
+  "surajmtp9@gmail.com", 
   "this message is from python")
 server.quit()
