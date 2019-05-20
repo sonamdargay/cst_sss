261144541aa67209b8149ffb14bc2e86ade7d595
suraj <suraj@sumukus.com>
2019-05-20 23:03:45 +0600

    updated gui

--- gui.py
+++ gui.py
@@ -1,10 +1,13 @@
 import sys
 from os import path
-import smtplib
+import sqlite3
+import datetime
+from time import clock
+#import smtplib
 
 import cv2
 import numpy as np
-sys.path.append('gui/pyCode/')
+sys.path.append('gui/')
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
@@ -68,7 +71,7 @@ class FaceDetectionWidget(QtWidgets.QWidget):
         faces=value1[1]
         gray=value1[0]
         id=0
-        names = ['None','Suraj Mukhia'] 
+        names = [['Sonam Dargay','12345678912'],['Suraj Mukhia','21309000371']]
         for (x, y, w, h) in faces:
             img=cv2.rectangle(image_data,
                           (x, y),
@@ -80,13 +83,48 @@ class FaceDetectionWidget(QtWidgets.QWidget):
             # Check if confidence is less them 100 ==> "0" is perfect match for 'confidence = "  {0}%".format(round(100 - confidence))'
             # Confidence 
             if (confidence < 100):
-                id = names[id]
+                ids=id
+                id = names[id][0]
                 #confidence = "  {0}%".format(round(100 - confidence))
                 confidence = "  {0}%".format(round(confidence))
+
+                #We are storing the time stamp of person whenever recognised.
+                connection = sqlite3.connect('mySSS.db')
+                cur = connection.cursor()
+
+                time_stamp = datetime.datetime.now()
+                id_number=names[ids][1]
+
+                cur.execute("SELECT * FROM Time_Log ORDER BY count DESC LIMIT 1")
+                result = cur.fetchone()
+                if result[0] != str(id_number):
+                    cur.execute('''INSERT INTO Time_Log(cid,time_stamp) VALUES(?,?)''',(id_number,time_stamp))
+                    connection.commit()
+                    connection.close()
+
             else:
                 id = "unknown"
                 #confidence = "  {0}%".format(round(100 - confidence))
                 confidence = "  {0}%".format(round(100 - confidence))
+
+                time=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+                cv2.imwrite('unknown/unknown'+"_"+str(time)+".jpg",gray[y:y+h,x:x+w])
+
+                #We are storing the time stamp of person whenever recognised.
+                connection = sqlite3.connect('mySSS.db')
+                cur = connection.cursor()
+
+                time_stamp = datetime.datetime.now()
+                id_number=id
+
+                cur.execute("SELECT * FROM Time_Log ORDER BY count DESC LIMIT 1")
+                result = cur.fetchone()
+                if result[0] != str(id_number):
+                    cur.execute('''INSERT INTO Time_Log(cid,time_stamp) VALUES(?,?)''',(id_number,time_stamp))
+                    connection.commit()
+                    connection.close()
+
+                '''
                 if id=="unknown":
                     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
                     server.login("cst.sss101@gmail.com", "Raspberrypi")
@@ -95,6 +133,7 @@ class FaceDetectionWidget(QtWidgets.QWidget):
                     "0215538.cst@rub.edu.bt", 
                     "Unknown FOund!")
                     server.quit()
+                '''
             cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
             cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
 
@@ -152,6 +191,7 @@ class MainWidget(QtWidgets.QWidget):
 
     def __init__(self, haarcascade_filepath, parent=None):
         super().__init__(parent)
+
         fp = haarcascade_filepath
         self.face_detection_widget = FaceDetectionWidget(fp)
 
--- gui/pyCode/database.py
+++
@@ -1,3 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('mySSS.db')
-cur = conn.cursor()
