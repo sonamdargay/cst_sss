7d82fbc3a5d3e37028f61a9cdf9cb82433e4a9de
SvnvmD <31983020+SvnvmD@users.noreply.github.com>
2019-08-31 11:22:48 +0600

    deleted

--- mail.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("cst.sss101@gmail.com", "Raspberrypi")
-server.sendmail(
-  "cst.sss101@gmail.com", 
-  "surajmtp9@gmail.com", 
-  "this message is from python")
-server.quit()
